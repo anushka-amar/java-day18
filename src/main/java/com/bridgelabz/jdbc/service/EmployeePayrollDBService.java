385d2350d91f2a41a01e04a87f0c9acc148f719f
Anushka <anushkamar1102@gmail.com>
2023-12-15 23:16:59 +0530

    Refactored: established connection with the db

--- src/main/java/com/bridgelabz/jdbc/service/EmployeePayrollDBService.java
+++ src/main/java/com/bridgelabz/jdbc/service/EmployeePayrollDBService.java
@@ -1,1 +1,23 @@
+package com.bridgelabz.jdbc.service;
 
+import com.bridgelabz.jdbc.entity.EmployeePayrollData;
+
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeePayrollDBService {
+
+    /* UC1 - established connection */
+    private static Connection getConnection() throws SQLException {
+        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+        String userName = "root";
+        String password = "kiit";
+        Connection connection;
+        System.out.println("Connecting to database: "+jdbcURL);
+        connection = DriverManager.getConnection(jdbcURL, userName, password);
+        System.out.println("Connection is Successful!!!"+connection);
+        return connection;
+    }
+}
